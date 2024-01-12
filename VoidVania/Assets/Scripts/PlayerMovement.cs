bd13da70c024aee5fb09a3907f7d03936b7ac574
Lucas Souza <lucassouzzamarques@gmail.com>
2024-01-11 22:14:26 -0300

    Horizontal player movement

--- VoidVania/Assets/Scripts/PlayerMovement.cs
+++ VoidVania/Assets/Scripts/PlayerMovement.cs
@@ -6,20 +6,29 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
+    Rigidbody2D rb;
+
+    [SerializeField] float runSpeed = 10f;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        Run();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb.velocity.y);
+        rb.velocity = playerVelocity;
     }
 }
