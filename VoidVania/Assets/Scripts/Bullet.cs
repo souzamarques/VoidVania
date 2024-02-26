af2cf255613bbec6719b320ac7161043f0cd418c
Lucas Souza <lucassouzzamarques@gmail.com>
2024-02-25 23:15:44 -0300

    Bullet behavior and killing Pink Guy

---
+++ VoidVania/Assets/Scripts/Bullet.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody2D rb;
+    PlayerMovement player;
+
+    float xSpeed;
+
+    [SerializeField] float bulletSpeed = 20f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<PlayerMovement>();
+        xSpeed = player.transform.localScale.x * bulletSpeed;
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector2(xSpeed, 0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Enemy")
+            Destroy(other.gameObject);
+
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+}
