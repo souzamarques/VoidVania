eff6598b79bf8b59c1a21346f06c4d99398e28fa
Lucas Souza <lucassouzzamarques@gmail.com>
2024-03-17 23:27:49 -0300

    Level Exit Portal with Coroutine

--- VoidVania/Assets/Scripts/LevelExit.cs
+++ VoidVania/Assets/Scripts/LevelExit.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
+    float levelLoadDelay = 2f;
+
     void OnTriggerEnter2D(Collider2D other)
     {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
